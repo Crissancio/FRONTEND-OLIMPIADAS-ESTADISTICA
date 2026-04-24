d1cb56bbfeccb86be2c75fb148743ad3abb9d5d8
Crissancio <soyelpapu1509@gmail.com>
2026-04-23 21:20:03 -0400

    version-base-configuracion-proyecto

---
+++ app/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+//import HelloWorld from './components/HelloWorld.vue'
+</script>
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <h1 class="text-3xl font-bold text-primary">
+      Proyecto limpio 🚀
+    </h1>
+  </div>
+</template>
---
+++ app/src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import '@fontsource/inter'
+import './style.css'
+
+import App from './App.vue'
+
+
+createApp(App).mount('#app')
