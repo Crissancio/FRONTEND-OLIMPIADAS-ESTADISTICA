46931840d67d6a518110fdeab70037113ea4c58a
Crissancio <soyelpapu1509@gmail.com>
2026-05-24 22:07:37 -0400

    ajustes mejores de transicion

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,7 +5,7 @@ import { Toaster } from 'vue-sonner'
 <template>
   <router-view v-slot="{ Component, route }">
     <transition name="route-fade" mode="out-in">
-      <component :is="Component" :key="route.fullPath" />
+      <component :is="Component" :key="route.path.startsWith('/admin') && route.path !== '/admin/login' ? 'admin-layout' : route.matched[0]?.path === '/' ? 'public-layout' : route.fullPath" />
     </transition>
   </router-view>
   <Toaster position="top-right" rich-colors />
